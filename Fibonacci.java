45eedcb6efd622fcc90ec2340e8def2dac62d47b
Ziyang Dai <61992255+ZiyangDai@users.noreply.github.com>
2020-03-12 23:25:29 +0100

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Fib();
+	}
+	
+	public static void Fib() {
+		int a = 0;
+		int b = 1; //The first two numbers
+		int c; //the sum of the first two numbers
+		System.out.println(a);
+		System.out.println(b);
+		for (int i=3;i<=20;i++) {
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c; //Continue the above loop
+		}
+		
+	}
+}
